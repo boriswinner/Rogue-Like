2c82a881bb98ccd052c1480939f1d1be344c0fad
boriswinner <timbori@mail.ru>
2018-04-05 00:21:57 +1000

    draft

--- game_objects.h
+++ game_objects.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <memory>
 #ifndef ROG_GAMEOBJECTS_H
 #define ROG_GAMEOBJECTS_H
 #endif //ROG_GAMEOBJECTS_H
@@ -9,11 +10,16 @@ typedef struct Point {
 
 using namespace std;
 
+class Map;
+
 class MapObject{
 protected:
     pnt position_;
     const char image_;
 public:
+    explicit MapObject( pnt position, char image = '*'):image_(image),position_(position){
+    }
+
     pnt get_position() const{
         return position_;
     }
@@ -24,12 +30,17 @@ public:
 };
 
 class Character: public MapObject{
-private:
-    pnt position_;
+protected:
     int hp_;
     int damage_;
 
 public:
+    Character(pnt position, int hp, int damage, char image) : MapObject(position,image) {
+        position_ = position;
+        hp_ = hp;
+        damage_ = damage;
+    }
+
     int get_hp() const{
         return hp_;
     }
@@ -50,21 +61,12 @@ public:
         return image_;
     }
 
-    void move(Map& map, const int xoffset, const int yoffset){
-        vector <vector <vector <MapObject*>>> &cells = map.get_map();
-        for (int i = 0; i < cells[position_.x][position_.y].size(); ++i){
-            if (cells[position_.x][position_.y][i] == this){
-                cells[position_.x][position_.y].erase(cells[position_.x][position_.y].begin() + i);
-            }
-        }
-        position_.x += xoffset;
-        position_.y += yoffset;
-        map.add_to_cell(position_.x,position_.y,this);
-    }
+    void move(Map& map, const int xoffset, const int yoffset);
 };
 
 class Player: public Character{
-    const char image_ = '*';
+public:
+    Player(pnt position, int hp, int damage) : Character(position, hp, damage, 'P'){}
 };
 
 class Map{
@@ -72,14 +74,14 @@ private:
     const int sizex_,sizey_;
     vector <vector <vector <MapObject*>>> cells;
 public:
-    Map(const unsigned int sizex, const unsigned int sizey, vector <MapObject> &objects): sizex_(sizex), sizey_(sizey){
-        vector <vector <vector <MapObject*>>> cells = vector <vector <vector <MapObject*>>>(sizex);
+    Map(const unsigned int sizex, const unsigned int sizey, vector<shared_ptr<MapObject>> &objects): sizex_(sizex), sizey_(sizey){
+        vector <vector <vector <shared_ptr<MapObject>>>> cells = vector <vector <vector <shared_ptr<MapObject>>>>(sizex);
         for (int i = 0; i < sizex; ++i){
-            cells[i].reserve(sizey);
+            cells[i].resize(sizey);
         }
-        for (int i = 0; i <= objects.size(); ++i){
-            pnt pos = objects[i].get_position();
-            cells[pos.x][pos.y].push_back(&objects[i]);
+        for (int i = 0; i < objects.size(); ++i){
+            pnt pos = objects[i]->get_position();
+            cells[pos.x][pos.y].push_back(objects[i]);
         }
     }
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include "curses.h"
 #include "game_objects.h"
+#include <vector>
+#include <memory>
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
-    initscr();                    /* Start curses mode */
-    printw("Hello World !!!");    /* Print Hello World */
-    refresh();                    /* Print it on to the real screen */
-    getch();                      /* Wait for user input */
-    endwin();                     /* End curses mode */
+
+    //vector <MapObject> objects {main_player};
+    vector<shared_ptr<MapObject>> objs;
+    objs.push_back(make_shared<Player>(Player({1,2},1,1)));
+    cout << objs[0]->get_position().x;
+    Map map(10,10,objs);
+    map.get_map();
+
 
     return 0;
 }
