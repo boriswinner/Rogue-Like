85f1bb862ad4351d796b754b1193dc9f5c930b21
boriswinner <timbori@mail.ru>
2018-06-02 18:21:57 +1000

    Added GameManager and blank for MapEditor, also  collides with walls don't work

---
+++ game_manager.h
@@ -0,0 +1,85 @@
+#include "game_objects.h"
+#include "curses.h"
+
+class GameManager{
+public:
+    explicit GameManager(const string &mapfilename): map_(mapfilename), cells_(map_.get_map()){}
+
+    void start_game(){
+        init_curses();
+    }
+
+    void make_move(){
+        map_.recontruct();
+        draw_map();
+        int key = getch();
+        if (key == 's') {
+            map_.player->move(map_, 0, 1);
+        } else if (key == 'w') {
+            map_.player->move(map_, 0, -1);
+        } else if (key == 'a') {
+            map_.player->move(map_, -1, 0);
+        } else if (key == 'd') {
+            map_.player->move(map_, 1, 0);
+        }
+
+        for (int i = 0; i < cells_.size(); i++) {
+            for (int j = 0; j < cells_[i].size(); j++) {
+                for (int z = 0; z < cells_[i][j].size(); z++) {
+                    cells_[i][j][z]->move(map_, 0, 0);
+                }
+            }
+        }
+        map_.recontruct();
+        for (int i = 0; i < cells_.size(); i++) {
+            for (int j = 0; j < cells_[i].size(); j++) {
+                for (int z = 0; z < cells_[i][j].size(); z++) {
+                    if (z < cells_[i][j].size() - 1) {
+                        for (int f = z + 1; f < cells_[i][j].size(); ++f) {
+                            cells_[i][j][z]->collide(*cells_[i][j][f]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void start_mapeditor(){
+
+    }
+protected:
+    Map map_;
+    vector<vector<vector<shared_ptr<MapObject>>>> &cells_;
+
+    void init_curses(){
+        initscr();
+        raw();
+        noecho();
+        curs_set(0);
+    }
+
+    void draw_hp(){
+        char player_hp[15] = "Player HP: ";
+        itoa(map_.player->get_hp(), player_hp + 11, 10); //11 is player_hp length
+        mvprintw(LINES - 1, 1, player_hp);
+        if (map_.player->get_hp() <= 0) {
+            mvprintw(LINES - 1, 1, "GAME OVER! PRESS R TO RESET");
+        }
+    }
+
+    void get_input(){
+
+    }
+    void draw_map(){
+        clear();
+        refresh();
+        vector<vector<vector<shared_ptr<MapObject>>>> &cells = map_.get_map();
+        for (int i = 0; i < cells.size(); i++) {
+            for (int j = 0; j < cells[i].size(); j++) {
+                addch(static_cast<const chtype>(cells[i][j][0]->get_image()));
+            }
+            addch('\n');
+        }
+        draw_hp();
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,64 +1,13 @@
 #include "curses.h"
 #include "game_objects.h"
+#include "game_manager.h"
 
 int main() {
+    GameManager game_manager("level1.txt");
 
-    Map map("level1.txt");
-
-    initscr();
-    raw();
-    noecho();
-    curs_set(0);
-
+    game_manager.start_game();
     while (true) {
-        clear();
-        refresh();
-        map.recontruct();
-        vector<vector<vector<shared_ptr<MapObject>>>> &cells = map.get_map();
-
-        for (int i = 0; i < cells.size(); i++) {
-            for (int j = 0; j < cells[i].size(); j++) {
-                addch(static_cast<const chtype>(cells[i][j][0]->get_image()));
-            }
-            addch('\n');
-        }
-        char player_hp[15] = "Player HP: ";
-        itoa(map.player->get_hp(), player_hp + 11, 10); //11 is player_hp length
-        mvprintw(LINES - 1, 1, player_hp);
-        if (map.player->get_hp() <= 0) {
-            mvprintw(LINES - 1, 1, "GAME OVER! PRESS R TO RESET");
-        }
-        refresh();
-        int key = getch();
-        if (key == 's') {
-            map.player->move(map, 0, 1);
-        } else if (key == 'w') {
-            map.player->move(map, 0, -1);
-        } else if (key == 'a') {
-            map.player->move(map, -1, 0);
-        } else if (key == 'd') {
-            map.player->move(map, 1, 0);
-        }
-
-        for (int i = 0; i < cells.size(); i++) {
-            for (int j = 0; j < cells[i].size(); j++) {
-                for (int z = 0; z < cells[i][j].size(); z++) {
-                    cells[i][j][z]->move(map, 0, 0);
-                }
-            }
-        }
-        map.recontruct();
-        for (int i = 0; i < cells.size(); i++) {
-            for (int j = 0; j < cells[i].size(); j++) {
-                for (int z = 0; z < cells[i][j].size(); z++) {
-                    if (z < cells[i][j].size() - 1) {
-                        for (int f = z + 1; f < cells[i][j].size(); ++f) {
-                            cells[i][j][z]->collide(*cells[i][j][f]);
-                        }
-                    }
-                }
-            }
-        }
+        game_manager.make_move();
     }
     endwin();
 
