efeb1b480ee3c8dba4010a042ec27fb1124958f0
boriswinner <timbori@mail.ru>
2018-06-07 00:15:29 +1000

    Improver MapEditor functionality and changed object orger in input

--- game_manager.h
+++ game_manager.h
@@ -85,7 +85,7 @@ protected:
         refresh();
         for (int i = 0; i < cells_.size(); i++) {
             for (int j = 0; j < cells_[i].size(); j++) {
-                addch(static_cast<const chtype>(cells_[i][j][0]->get_image()));
+                addch(static_cast<const chtype>(cells_[i][j][cells_[i][j].size()-1]->get_image()));
             }
             addch('\n');
         }
@@ -110,20 +110,56 @@ protected:
     void check_keys(int key) override {
         GameManager::check_keys(key);
         if (key == '1') {
-            for (int i = 0; i < cells_[map_.player->get_position().y][map_.player->get_position().x].size(); ++i) {
-                cells_[map_.player->get_position().y][map_.player->get_position().x][i]->remove();
-            }
-            map_.player->deremove();
-            map_.add_object(make_shared<Wall>(pnt{map_.player->get_position().x, map_.player->get_position().y}, '#'));
-            map_.add_object(make_shared<Floor>(pnt{map_.player->get_position().x, map_.player->get_position().y}, '*'));
+            replace_player_cell(make_shared<Wall>(pnt{map_.player->get_position().x, map_.player->get_position().y}, '#'));
+        } else if (key == '2'){
+            echo();
+            mvprintw(LINES - 2, 1, "Enter Monster HP:");
+            char hp_str[10];
+            getstr(hp_str);
+            int hp = strtol(hp_str, nullptr,0);
+            if (hp == 0L)
+                return;
+            mvprintw(LINES - 2, 1, "Enter Monster damage:");
+            char dmg_str[10];
+            getstr(dmg_str);
+            int dmg = strtol(dmg_str, nullptr,0);
+            if (dmg == 0L)
+                return;
+            noecho();
+            replace_player_cell(make_shared<Monster>(pnt{map_.player->get_position().x, map_.player->get_position().y}, hp,dmg,'&'));
+        } else if (key == '3'){
+            echo();
+            mvprintw(LINES - 2, 1, "Enter Healer Heal Value:");
+            char hp_str[10];
+            getstr(hp_str);
+            int hp = strtol(hp_str, nullptr,0);
+            if (hp == 0L)
+                return;
+            replace_player_cell(make_shared<Healer>(pnt{map_.player->get_position().x, map_.player->get_position().y}, hp,'@'));
+            noecho();
+        } else if (key == '4'){
+            clear_player_cell();
         }
+    }
+
+    void replace_player_cell(const shared_ptr<MapObject> &obj) {
+        clear_player_cell();
+        map_.add_object(obj);
+    }
+
+    void clear_player_cell(){
+        for (int i = 0; i < cells_[map_.player->get_position().y][map_.player->get_position().x].size(); ++i) {
+            cells_[map_.player->get_position().y][map_.player->get_position().x][i]->remove();
+        }
+        map_.player->deremove();
+        map_.add_object(make_shared<Floor>(pnt{map_.player->get_position().x, map_.player->get_position().y}, '*'));
     }
 
     void draw_ui() override {
         attron(COLOR_PAIR(2));
         mvprintw(1, map_.getsize().x + 1, "P is cursor");
         attron(COLOR_PAIR(3));
-        mvprintw(2, map_.getsize().x + 1, "1 - wall, 2 - monster, 3 - healer");
+        mvprintw(2, map_.getsize().x + 1, "1 - wall, 2 - monster, 3 - healer, 4- clear");
         attron(COLOR_PAIR(1));
     }
 
