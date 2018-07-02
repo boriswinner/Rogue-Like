db8c1f3c86dc8c0ff2f2d9c9298e9384ceec959b
boriswinner <timbori@mail.ru>
2018-07-03 02:59:46 +1000

    Demo realtome

--- user_interface.cpp
+++ user_interface.cpp
@@ -1,5 +1,11 @@
+#include <ratio>
+#include <chrono>
+#include <unistd.h>
+#include <winuser.h>
 #include "user_interface.h"
 #include "config.h"
+#include "thread"
+#include "mingw.thread.h"
 
 void MainMenuController::run() {
     Menu main_menu(string("Rogue-Like"), 3, string("Play Game"), string("Open Map Editor"), string("Exit"));
@@ -24,10 +30,39 @@ void GameController::run() {
     GameManager game_manager(mapfilename_);
     game_manager.start_game();
     GameDrawHandler game_draw_handler(game_manager.get_map());
+    std::chrono::duration<long, std::ratio<5, 25>> frame_ratio{ 1 };
+    nodelay(stdscr, TRUE);
     while (game_manager.game_status() == isrunning) {
         game_draw_handler.draw_map();
-        int key = getch();
-        game_manager.make_move(key);
+        while (true) {
+            if (GetAsyncKeyState(VK_LEFT)) {
+                game_manager.make_move(game_config.data["ButtonLeft"]);
+            } else if (GetAsyncKeyState(VK_RIGHT)){
+                game_manager.make_move(game_config.data["ButtonRight"]);
+            } else if (GetAsyncKeyState(VK_DOWN)){
+                game_manager.make_move(game_config.data["ButtonDown"]);
+            } else if (GetAsyncKeyState(VK_UP)){
+                game_manager.make_move(game_config.data["ButtonUp"]);
+            } else {
+                game_manager.make_move(game_config.data["NotUsedKey"]);
+            }
+            game_draw_handler.draw_map();
+            std::this_thread::sleep_for(100ms);
+            }
+        while(int key = getch()){
+            auto frame_start = std::chrono::steady_clock::now();
+            if (key > 0){
+                game_manager.make_move(key);
+            } else{
+                game_manager.make_move(game_config.data["NotUsedKey"]);
+            }
+            game_draw_handler.draw_map();
+            auto frame_end = std::chrono::steady_clock::now();
+            auto delay = frame_ratio - (frame_end - frame_start);
+            std::this_thread::sleep_for(delay);
+            //flushinp();
+        }
+
     }
     if (game_manager.game_status() == defeat){
         game_draw_handler.draw_defeat();
